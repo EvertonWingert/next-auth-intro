f60fbbb442c477df747c1cb139d570903301fb22
Everton <Evertonwingert@outlook.com>
2023-10-24 20:57:40 -0300

    spotify

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,39 +1,15 @@
 import type { NextAuthOptions } from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
-import CredentialsProvider from 'next-auth/providers/credentials'
+import SpotifyProvider from 'next-auth/providers/spotify'
+
 
 export const options: NextAuthOptions = {
+    secret: process.env.AUTH_SECRET,
     providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                username: {
-                    label: "Username:",
-                    type: "text",
-                    placeholder: "your-cool-username"
-                },
-                password: {
-                    label: "Password:",
-                    type: "password",
-                    placeholder: "your-awesome-password"
-                }
-            },
-            async authorize(credentials) {
-                // This is where you need to retrieve user data 
-                // to verify with credentials
-                // Docs: https://next-auth.js.org/configuration/providers/credentials
-                const user = { id: "42", name: "Dave", password: "nextauth" }
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
 
-                if (credentials?.username === user.name && credentials?.password === user.password) {
-                    return user
-                } else {
-                    return null
-                }
-            }
-        })
+        }),
     ],
 }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,16 @@
 import { options } from "./api/auth/[...nextauth]/options"
 import { getServerSession } from "next-auth/next"
-import UserCard from "./components/UserCard"
 
 export default async function Home() {
   const session = await getServerSession(options)
 
+  if(!session) {
+    return <h1 className="text-5xl text-gray-900">Deslogado</h1>
+  }
+
   return (
     <>
-      {session ? (
-        <UserCard user={session?.user} pagetype={"Home"} />
-      ) : (
-        <h1 className="text-5xl">You Shall Not Pass!</h1>
-      )}
+      <h3 className="text-gray-900 text-5xl">{session.user?.name}</h3>
     </>
   )
 }
